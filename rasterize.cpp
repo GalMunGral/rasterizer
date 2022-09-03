905ec29c06d4bf74af376897a0179968b707346c
Wenqi He <hewenqi96@gmail.com>
2022-09-03 12:52:00 -0500

    clean up code

--- rasterize.cpp
+++ rasterize.cpp
@@ -75,28 +75,22 @@ void rasterizer::set_color(double _r, double _g, double _b)
 }
 
 template <class Operation>
-void dda_foreach(vertex a, vertex b, int i, Operation operate)
+void dda_foreach(vertex a, vertex b, int i, Operation f)
 {
     if (a[i] == b[i])
         return;
     if (a[i] > b[i])
         std::swap(a, b);
     auto s = (b - a) / (b[i] - a[i]);
-    std::cout << "A: " << a << '\n'
-              << "B: " << b << '\n'
-              << "S: " << s << '\n';
     for (auto p = a + (std::ceil(a[i]) - a[i]) * s; p[i] < b[i]; p += s)
     {
-        std::cout << p;
-        operate(p);
+        f(p);
     }
 }
 
 void rasterizer::draw_triangle(display &disp, int i1, int i2, int i3)
 {
     std::vector<vertex> vertices{ith_vertex(i1), ith_vertex(i2), ith_vertex(i3)};
-    sort(vertices.begin(), vertices.end(), [](vertex &a, vertex &b)
-         { return a[1] <= b[1]; });
     for (auto &v : vertices)
     {
         double w = v[3];
@@ -108,46 +102,36 @@ void rasterizer::draw_triangle(display &disp, int i1, int i2, int i3)
         v[0] = (v[0] / w + 1) * disp.width / 2;
         v[1] = (v[1] / w + 1) * disp.height / 2;
     }
+
+    sort(vertices.begin(), vertices.end(), [](vertex &a, vertex &b)
+         { return a[1] <= b[1]; });
+
     std::vector<vertex> bound1, bound2;
-    std::cout << "IT 0-2\n";
     dda_foreach(vertices[0], vertices[2], 1, [&](vertex &v)
                 { bound1.push_back(v); });
-    std::cout << "IT 0-1\n";
     dda_foreach(vertices[0], vertices[1], 1, [&](vertex &v)
                 { bound2.push_back(v); });
-    std::cout << "IT 1-2\n";
     dda_foreach(vertices[1], vertices[2], 1, [&](vertex &v)
                 { bound2.push_back(v); });
-    if (bound1.size() != bound2.size())
+
+    assert(bound1.size() == bound2.size());
+
+    auto draw_pixel = [&](vertex &v)
     {
-        throw std::range_error("??????");
-    }
-    // for (auto v : vertices)
-    // {
-    //     auto device_x = floor((v[0] + 1) * (disp.width - 1) / 2);
-    //     auto device_y = floor((v[1] + 1) * (disp.height - 1) / 2);
-    //     disp.set_color(device_x, device_y, v[4] / v[3], v[5] / v[3], v[6] / v[3], v[7] / v[3]);
-    // }
-    // for (size_t i = 0; i < bound1.size(); ++i)
-    // {
-    //     std::cout << "IT line\n";
-    //     dda_foreach(bound1[i], bound2[i], 0, [&](vertex &v)
-    //                 { disp.set_color(v[0], v[1], v[4], v[5], v[6], v[7]); });
-    // }
+        if (false)
+        {
+            for (size_t i = 4; i < v.size(); ++i)
+            {
+                v[i] /= v[3];
+            }
+            v[3] = 1 / v[3];
+        }
+        disp.set_color(v[0], v[1], v[4], v[5], v[6], v[7]);
+    };
+
     for (size_t i = 0; i < bound1.size(); ++i)
     {
-        std::cout << "IT line\n";
-        dda_foreach(bound1[i], bound2[i], 0, [&](vertex &v)
-                    {
-                        if (false)
-                        {
-                            for (size_t i = 4; i < v.size(); ++i)
-                            {
-                                v[i] /= v[3];
-                            }
-                            v[3] = 1 / v[3];
-                        }
-                        disp.set_color(v[0], v[1], v[4], v[5], v[6], v[7]); });
+        dda_foreach(bound1[i], bound2[i], 0, draw_pixel);
     }
 }
 
